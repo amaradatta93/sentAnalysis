45f8ce726ac59199b498145d62e48f564289d1c0
Amara Datta <amaradatta@gmail.com>
2018-09-03 13:40:43 -0700

    create a list of dict with extracted data

--- sentAnalysis.py
+++ sentAnalysis.py
@@ -1,31 +1,45 @@
 import pprint
 
-from utils import statuses_response, hashtags_response, convert_to_timestamp
+from utils import statuses_response, hashtags_response, convert_to_timestamp, hashtags
 
 tweets = []
 
 
 def status_tweets():
     response = statuses_response()
+    # pprint.pprint(response)
     for status in response:
-        tweets.append(status['full_text'])
-        stamp = convert_to_timestamp(status['created_at'])
-        tweets.append(stamp)
+        tweets.append({
+            'timestamp': convert_to_timestamp(status['created_at']),
+            'hashtag': None,
+            'author': status['user']['name'],
+            'tweet': status['full_text'],
+        })
+    return tweets
 
 
-'''
 def hashtag_tweets(index):
     response = hashtags_response(index)
     for hashtag in response['statuses']:
         if 'retweeted_status' in hashtag.keys():
-            tweets.append(hashtag['retweeted_status']['full_text'])
+            tweets.append({
+                'timestamp': convert_to_timestamp(hashtag['created_at']),
+                'hashtag': hashtags[index],
+                'author': hashtag['retweeted_status']['user']['name'],
+                'tweet': hashtag['retweeted_status']['full_text'],
+            })
         else:
-            tweets.append(hashtag['full_text'])
+            tweets.append({
+                'timestamp': convert_to_timestamp(hashtag['created_at']),
+                'hashtag': hashtags[index],
+                'author': hashtag['user']['name'],
+                'tweet': hashtag['full_text'],
+            })
 
 
-status_tweets()
-hashtag_tweets(1)
-pprint.pprint(set(tweets))
+# status_tweets()
+hashtag_tweets(0)
+pprint.pprint(tweets)
 
 # connection = psycopg2.connect("dbname=tweets user=sentanalysis")
 
@@ -44,4 +58,3 @@ pprint.pprint(set(tweets))
 
 # import inspect
 # print(inspect.signature(twitter.Api.__init__))
-'''
