7bdc6bc4baf51e1391f9da7624a00dfef1c4a1dd
Amara Datta <amaradatta@gmail.com>
2018-09-03 00:05:33 -0700

    implement fuctional method to setup multi-threading

--- sentAnalysis.py
+++ sentAnalysis.py
@@ -2,19 +2,27 @@ import pprint
 
 from utils import statuses_response, hashtags_response
 
-# tweets = []
-# 
-# for s in statuses_response:
-#     tweets.append(s['full_text'])
-#
-# for h in hashtags_response()['statuses']:
-#
-#     if 'retweeted_status' in h.keys():
-#         tweets.append(h['retweeted_status']['full_text'])
-#     else:
-#         tweets.append(h['full_text'])
-#
-# pprint.pprint(set(tweets))
+tweets = []
+
+
+def status_tweets():
+    response = statuses_response()
+    for status in response:
+        tweets.append(status['full_text'])
+
+
+def hashtag_tweets(index):
+    response = hashtags_response(index)
+    for hashtag in response['statuses']:
+        if 'retweeted_status' in hashtag.keys():
+            tweets.append(hashtag['retweeted_status']['full_text'])
+        else:
+            tweets.append(hashtag['full_text'])
+
+
+status_tweets()
+hashtag_tweets(1)
+pprint.pprint(set(tweets))
 
 # connection = psycopg2.connect("dbname=tweets user=sentanalysis")
 
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import os
-from urllib.parse import urlencode
+from urllib.parse import urlencode, quote
 
 import requests
 from requests_oauthlib import OAuth1
@@ -8,25 +8,26 @@ CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
 CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
+hashtags = ['#SpaceX', '#Tesla', '#Paypal']
 
 
-def url_response(url, **kwargs):
-    params = urlencode(kwargs)
+def url_response(url, entities):
+    params = urlencode(entities)
     url_final = url + params
-    print(url_final)
     auth = OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     return_response = requests.get(url_final, auth=auth)
+    print(return_response)
     return return_response.json()
 
 
 def statuses_response():
-    status_response = url_response('https://api.twitter.com/1.1/statuses/user_timeline.json?', screen_name='elonmusk',
-                                   tweet_mode='extended')
+    entities = dict(screen_name='elonmusk', tweet_mode='extended')
+    status_response = url_response('https://api.twitter.com/1.1/statuses/user_timeline.json?', entities)
+    print(status_response)
     return status_response
 
 
 def hashtags_response(index):
-    hashtags = ['#SpaceX', '#Tesla', '#Paypal']
-    hashtag_response = url_response('https://api.twitter.com/1.1/search/tweets.json?', q=hashtags[index], count=10,
-                                    result_type='recent', tweet_mode='extended')
+    entities = dict(q=hashtags[index], count=10,result_type='recent', tweet_mode='extended')
+    hashtag_response = url_response('https://api.twitter.com/1.1/search/tweets.json?', entities)
     return hashtag_response
